1ba29f71b712ce792aa221daaab21a4d5673e131
AbdoulieJBah <155037012+AbdoulieJBah@users.noreply.github.com>
2025-10-09 03:50:23 +0200

    Update App.py

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ import pickle
 # -------------------------
 # Load trained RandomForest GridSearchCV pipeline
 # -------------------------
-model = pickle.load(open('rf_grid.pkl', 'rb'))  # Make sure this file is in the same folder
+model = pickle.load(open('best_model_RandomForestClassifier.pkl', 'rb'))  # Make sure this file is in the same folder
 
 # -------------------------
 # Streamlit UI
